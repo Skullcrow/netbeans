af12c55609ac27aa3c250b3b57df4145a751867f
daniel <daniel@DESKTOP-SKULLCROW.mshome.net>
2020-11-02 14:32:11 -0600

    Algunas clases listas

---
+++ ProyectoFinal1/src/datos/AccesoDatosImpl.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datos;
+
+import control.AccesoDatosEx;
+import control.EscrituraDatosEx;
+import control.LecturaDatosEx;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import modelo.Pelicula;
+
+/**
+ *
+ * @author daniel
+ */
+public class AccesoDatosImpl implements IAccesoDatos{
+
+    @Override
+    public boolean existe(String nombre) throws AccesoDatosEx {
+        File archivo = new File(nombre);
+        return archivo.exists();        
+    }
+
+    @Override
+    public List<Pelicula> listar(String nombre) throws LecturaDatosEx {
+        var archivo = new File(nombre);
+        List<Pelicula> peliculas = new ArrayList<>();
+        try {
+            var entrada = new BufferedReader(new FileReader(archivo));
+            String linea = null;
+            linea = entrada.readLine();
+            while(linea != null){
+                var pelicula = new Pelicula(linea);
+                peliculas.add(pelicula);
+                linea = entrada.readLine();
+            }
+            entrada.close();
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+            throw new LecturaDatosEx("Excepcion al listar peliculas: " + ex.getMessage());
+        } catch (IOException ex) {
+            throw new LecturaDatosEx("Excepcion al listar peliculas: " + ex.getMessage());
+        }
+        return peliculas;
+    }
+
+    @Override
+    public void escribir(Pelicula pelicula, String archivo, boolean anexar) throws EscrituraDatosEx {
+    }
+
+    @Override
+    public String buscar(String archivo, String buscar) throws LecturaDatosEx {
+    }
+
+    @Override
+    public void crear(String archivo) throws AccesoDatosEx {
+    }
+
+    @Override
+    public void borrar(String archivo) throws AccesoDatosEx {
+    }
+    
+}
