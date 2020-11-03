26844538f25ea38cefa530f46affae27659959b7
daniel <daniel@DESKTOP-SKULLCROW.mshome.net>
2020-11-03 09:52:48 -0600

    ManejoDAO + Pull de Conexiones

--- ManejoDAO/src/main/java/acceso/Conexion.java
+++ ManejoDAO/src/main/java/acceso/Conexion.java
@@ -1,14 +1,27 @@
 package acceso;
 
 import java.sql.*;
+import javax.sql.DataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
 
 public class Conexion {
     private static final String JDBC_URL = "jdbc:mysql://localhost/test?useSSL=false&serverTimezone=UTC";
     private static final String JDBC_USER = "root";
     private static final String JDBC_PASS = "Skull@130187";
     
+    public static DataSource getDataSource(){
+        BasicDataSource ds = new BasicDataSource();
+        ds.setUrl(JDBC_URL);
+        ds.setUsername(JDBC_USER);
+        ds.setPassword(JDBC_PASS);
+        //Se define el tamano inicial del pool de conexiones
+        ds.setInitialSize(5);
+        return ds;
+    }
+    
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASS);
+        //return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASS);
+        return getDataSource().getConnection();
     }
     
     public static void close(ResultSet rs){
